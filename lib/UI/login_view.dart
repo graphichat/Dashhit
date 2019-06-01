223069e2e7fbc65646b9212c1f5053c8271f443f
Shashikant <shashikant@shaale.com>
2019-06-01 14:55:48 +0530

    State management # Auth # Message sending

--- lib/UI/login_view.dart
+++ lib/UI/login_view.dart
@@ -9,6 +9,7 @@ class LoginView extends StatefulWidget {
 
 class _LoginView extends State<LoginView> with SingleTickerProviderStateMixin {
   AuthBLoc authBloc;
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   void initState() {
     super.initState();
@@ -18,60 +19,76 @@ class _LoginView extends State<LoginView> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(top: 50),
-              child: Image.asset(
-                "assets/images/Group.png",
-                width: 200,
+      key: scaffoldKey,
+      body: BlocListener(
+        bloc: authBloc,
+        listener: (ctx, state) {
+          if (state is LoggedInS) {
+            Navigator.of(ctx).push(MaterialPageRoute(builder: (context) {
+              return null; //TODO;
+            }));
+          }
+          if (state is LoginErrorS) {
+            scaffoldKey.currentState.showSnackBar(SnackBar(
+              content: Text('Something really went wrong!!'),
+            ));
+          }
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                child: Image.asset(
+                  "assets/images/Group.png",
+                  width: 200,
+                ),
               ),
-            ),
-            RaisedButton(
-              child: BlocBuilder(
-                bloc: authBloc,
-                builder: (BuildContext context, AuthState state) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      AnimatedCrossFade(
-                        duration: Duration(milliseconds: 360),
-                        crossFadeState: state is LoggingInS
-                            ? CrossFadeState.showFirst
-                            : CrossFadeState.showSecond,
-                        firstChild:
-                            Image.asset('assets/images/g-logo.gif', height: 32),
-                        secondChild: Image.asset(
-                          'assets/images/google_icon.png',
-                          height: 32,
-                        ),
-                      ),
-                      Image.asset(
-                        'assets/images/google_icon.png',
-                        height: 28,
-                      ),
-                      Container(
-                        width: 186,
-                        padding: EdgeInsets.only(left: 12),
-                        child: Text(
-                          'Sign in with Google',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
+              RaisedButton(
+                child: BlocBuilder(
+                  bloc: authBloc,
+                  builder: (BuildContext context, AuthState state) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        AnimatedCrossFade(
+                          duration: Duration(milliseconds: 360),
+                          crossFadeState: state is LoggingInS
+                              ? CrossFadeState.showFirst
+                              : CrossFadeState.showSecond,
+                          firstChild: Image.asset('assets/images/g-logo.gif',
+                              height: 32),
+                          secondChild: Image.asset(
+                            'assets/images/google_icon.png',
+                            height: 32,
                           ),
                         ),
-                      )
-                    ],
-                  );
-                },
-              ),
-              onPressed: () => authBloc.dispatch(LoginWithGoogleE()),
-            )
-          ],
+                        Image.asset(
+                          'assets/images/google_icon.png',
+                          height: 28,
+                        ),
+                        Container(
+                          width: 186,
+                          padding: EdgeInsets.only(left: 12),
+                          child: Text(
+                            'Sign in with Google',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                ),
+                onPressed: () => authBloc.dispatch(LoginWithGoogleE()),
+              )
+            ],
+          ),
         ),
       ),
     );
