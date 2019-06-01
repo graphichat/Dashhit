ec7e243eb82f2fa7c2c97a41e0ab090cfef7b4c3
graphichat <praveen.nalakurthi>
2019-06-01 10:35:13 +0530

    Created UI Folder

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:dashhit/UI/MainPage_View.dart';
+
+void main() => runApp(MainApp());
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
