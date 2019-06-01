2858eb9b8cb380f41260ccd3516e72177a609a10
Shashikant <shashikant@shaale.com>
2019-06-01 14:47:51 +0530

    State management # Auth # Message sending

--- lib/blocs/auth/bloc.dart
+++ lib/blocs/auth/bloc.dart
@@ -3,6 +3,7 @@ library auth_bloc;
 import 'package:bloc/bloc.dart';
 import 'package:dashhit/api/auth_api.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:meta/meta.dart';
 
 part 'src/auth_bloc.dart';
--- lib/blocs/auth/src/auth_bloc.dart
+++ lib/blocs/auth/src/auth_bloc.dart
@@ -2,6 +2,10 @@ part of auth_bloc;
 
 ///This bloc handles all the states of the authentication
 class AuthBLoc extends Bloc<AuthEvent, AuthState> {
+  AnimationController btnSizeController;
+  final TickerProvider vsync;
+
+  AuthBLoc(this.vsync);
   @override
   AuthState get initialState =>
       AuthApi.user != null ? LoggedInS(AuthApi.user) : Loggedout();
@@ -10,12 +14,20 @@ class AuthBLoc extends Bloc<AuthEvent, AuthState> {
   Stream<AuthState> mapEventToState(AuthEvent event) async* {
     if (event is LoginWithGoogleE) {
       yield LoggingInS();
+      btnSizeController.reverse();
       try {
         final user = await AuthApi().loginWithGoogle();
         yield user != null ? LoggedInS(user) : LoginErrorS();
       } catch (err) {
         yield LoginErrorS();
       }
+      btnSizeController.forward();
     }
+  }
+
+  @override
+  void dispose() {
+    btnSizeController.dispose();
+    super.dispose();
   }
 }
